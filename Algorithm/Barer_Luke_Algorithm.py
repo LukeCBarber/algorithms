ff01dd97a24d3290c987eea65d3fa92de66da9a0
Luke <141974348+LukeCBarber@users.noreply.github.com>
2023-09-23 16:53:24 -0400

    Add files via upload

---
+++ Algorithm/Barer_Luke_Algorithm.py
@@ -0,0 +1,98 @@
+#Brushing my teeth
+print("THE TOOTHBRUSHING ALGORITHM - Luke Barber")
+print("")
+
+#grab toothbrush
+grab_brush = "Grab toothbrush using left hand"
+print("Step 1:", grab_brush)
+print("")
+
+#grab toothpaste
+grab_paste = "Grab toothpaste with right hand"
+print("Step 2:", grab_paste)
+print("")
+
+#rotate toothpaste horizontal
+rotate_paste = "Rotate toothpaste to a horizontal position"
+print("Step 3:", rotate_paste)
+print("")
+
+#squeeze toothpaste
+squeeze_start = "Gently squeeze toothpaste"
+squeeze_stop = "After .5 seconds, stop squeezing"
+print("Step 4:", squeeze_start)
+print("")
+print("Step 5:", squeeze_stop)
+print("")
+
+#drop toothpaste
+drop_paste = "Drop toothpaste"
+print("step 6:", drop_paste)
+print("")
+
+#raise toothbrush to mouth
+raise_brush = "Raise toothbrush to mouth"
+print("Step 7:",raise_brush)
+print("")
+
+#smile
+smile = "Smile"
+print("Step 8:", smile)
+print("")
+
+#place toothbrush on teeth
+top_place = "Place toothbrush on top teeth row"
+print("Step 9:", top_place)
+print("")
+
+#move toothbrush in circular motion
+brush_motion = "Move toothbrush in small circular motions"
+repeat_brush = "Repeat Step 10 continuously"
+print("Step 10:", brush_motion)
+print("")
+print("Step 11:", repeat_brush)
+print("")
+
+#move toothbrush along teeth (Top left)
+topleft_start = "Slide toothbrush left along the teeth"
+topleft_stop = "At the back left molar, stop sliding"
+print("Step 12:", topleft_start)
+print("")
+print("Step 13:", topleft_stop)
+print("")
+
+#move toothbrush along teeth (Top Right)
+topright_start = "Slide toothbrush right along the teeth"
+topright_stop = "At the back right molar, stop sliding"
+print("Step 14:", topright_start)
+print("")
+print("Step 15:", topright_stop)
+print("")
+
+#move to bottom row
+lift_brush = "lift toothbrush off of teeth"
+bottom_place = "Place toothbrush on bottom teeth row"
+print("Step 16:", lift_brush)
+print("")
+print("Step 17:", bottom_place)
+print("")
+
+#move tootrush along teeth (Bottom Left)
+bottomleft_start = "Slide toothbrush left along the teeth"
+bottomleft_stop = "At the back left molar, stop sliding"
+print("Step 18:", bottomleft_start)
+print("")
+print("Step 19:", bottomleft_stop)
+print("")
+
+#move toothbrush along teeth (Bottom Right)
+bottomright_start = "Slide toothbrush right along the teeth"
+bottomright_stop = "At the back right molar, stop sliding"
+print("Step 20:", bottomright_start)
+print("")
+print("Step 21:", bottomright_stop)
+print("")
+
+#end
+print("THE END :)")
+
